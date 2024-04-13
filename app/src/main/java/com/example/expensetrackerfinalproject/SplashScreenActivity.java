3634d46edd9c2025329ecb52cddfe671900b1c7b
zakiahmed7 <syedzakiahmed711@gmail.com>
2024-04-13 12:05:17 -0400

    Added Login page and signup page

--- app/src/main/java/com/example/expensetrackerfinalproject/SplashScreenActivity.java
+++ app/src/main/java/com/example/expensetrackerfinalproject/SplashScreenActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginAndSignupActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_and_signup);
+        setContentView(R.layout.activity_splash_screen);
     }
 }
